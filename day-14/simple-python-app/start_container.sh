2f8bedb275a3c1444916dac38493c693fe769306
venkatk3691 <venkatk3691@gmail.com>
2024-10-28 17:32:30 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull "venkatk3691/simple-python-flask-app"
 
 # Run the Docker image as a container
-echo "Hi welcome"
+docker run -d -p 5000:5000 venkatk3691/simple-python-flask-app
